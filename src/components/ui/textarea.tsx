36e35d9bd18dff292943cc237ff7cd3741095975
leon-i <isakleon97@gmail.com>
2020-08-28 15:40:48 -0400

    Added textarea placeholders

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -9,7 +9,9 @@ interface Props {
 const Textarea : React.FC<Props> = ({ label, value, onChange }) => (
     <div className='ui-textarea'>
         <span>{label}</span>
-        <textarea value={value} onChange={onChange}></textarea>
+        <textarea value={value} 
+        onChange={onChange}
+        placeholder='KEYWORD TEXT STYLING - !keyword!&#10;CARD REFERENCE STYLING - #card#&#10;KEYWORD IMAGE - *keyword*'></textarea>
     </div>
 );
 
